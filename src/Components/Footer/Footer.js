310b952e957235c3a7df6a3926d8bb1dbecd5247
ADSM <yesform2018@gmail.com>
2022-04-04 03:27:34 +0600

    notfound page is completed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { HeartIcon } from '@heroicons/react/solid'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
+        <div className='bg-[#275ec2] flex items-center justify-center h-24 mt-72'>
+            <h1 className='flex text-yellow-50'>Created With <HeartIcon className='h-6 w-6' /> By Mr.Mads</h1>
         </div>
     );
 };
