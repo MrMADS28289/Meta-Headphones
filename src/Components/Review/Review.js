f1ea2540da0cf6e5ebdfa79b8f08100cd0043034
ADSM <yesform2018@gmail.com>
2022-04-04 06:50:16 +0600

    review part is completed

---
+++ src/Components/Review/Review.js
@@ -0,0 +1,28 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import Rating from 'react-rating';
+
+
+const Review = (props) => {
+
+    const { name, img, rating, review } = props.review;
+
+    return (
+        <div className='m-4 p-6 border-2 border-gray-200 bg-[#FAFAFA] flex flex-col items-center'>
+            <div>
+                <img className='rounded-[50%] h-14 w-14' src={img} alt="" />
+            </div>
+            <h1 className='text-xl font-bold'>Name: {name}</h1>
+            <h5><Rating
+                initialRating={rating}
+                emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                fullSymbol={<FontAwesomeIcon style={{ color: '#ffd700' }} icon={faStar} />}
+                readonly
+            ></Rating> </h5>
+            <p className='text-lg'>{review}</p>
+        </div>
+    );
+};
+
+export default Review;
--- src/Components/Reviews/reviews.js
+++ src/Components/Reviews/reviews.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useReview from '../../Hooks/useReview';
+import Review from '../Review/Review';
 
 const reviews = () => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [reviews] = useReview();
+
+
+
     return (
-        <div>
-            <h1>reviews</h1>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {
+                reviews.map(review => <Review
+                    key={review.id}
+                    review={review}
+                ></Review>)
+            }
         </div>
     );
 };
