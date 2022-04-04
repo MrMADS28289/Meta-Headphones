60a0c7f39e951f5ff138c282008c2b32300f7fca
ADSM <yesform2018@gmail.com>
2022-04-04 18:04:52 +0600

    About part is completed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>about</h1>
-        </div>
+        <>
+            <div className='flex flex-col m-16 p-8 items-center bg-[#FAFAFA]'>
+                <h1 className='text-4xl font-bold'>About This Site</h1>
+                <p className='p-8 text-3xl'>This is a product analysis web-site.When building digital products, a lot of questions arise: What features do users want? How will they interact with the product? How can we make the user experience better?
+                    Product analytics tools provide answers to these questions and more. They allow product managers to assess their development efforts critically. Armed with the detailed insights such tools provide, product teams can then optimize performance, remove roadblocks, and increase customer satisfaction.
+
+                    In this article, I will go over 10 of the best product analysis tools on the market and explain what each does best. I hope this will help you narrow down your choices while shopping around. </p>
+            </div>
+            <div className='flex flex-col m-16 p-8 items-center bg-[#FAFAFA]'>
+                <h1 className='text-4xl font-bold'>My Product Analytics Solutions Comparison Criteria</h1>
+                <p className='p-8 text-3xl'>What do I look for when I select the best product analytics platforms? Here’s a summary of my evaluation criteria: <br /><span>1.User Interface (UI): I look for product analytics tools that surface important information to help product managers make critical decisions.</span><br /><span>2.Usability: Here I focus on features that allow all product team members to navigate and efficiently use the features they need.</span><br /><span>3.Integrations: I look for tight integrations with other tools that product development teams use daily.</span><br /><span>4.Value for $: I seek product analytics tools priced competitively compared to other development software.</span> </p>
+            </div>
+        </>
     );
 };
 
